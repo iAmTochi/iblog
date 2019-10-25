cffdacbf187ed17786cbb788adb901196269e560
Tochukwu <ugwukelvintochukwu@gmail.com>
2019-10-25 12:44:06 +0100

    Create post ui

--- database/migrations/2019_10_24_154603_create_posts_table.php
+++ database/migrations/2019_10_24_154603_create_posts_table.php
@@ -19,7 +19,7 @@ class CreatePostsTable extends Migration
 
             $table->string('title');
 
-            $table->string('description');
+            $table->text('description');
 
             $table->text('content');
 
