16ee01773d7c19aa33be821e8ba6d379ac209564
Tochukwu <ugwukelvintochukwu@gmail.com>
2019-10-25 15:27:26 +0100

    Displaying all posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,6 +12,32 @@
         <div class="card-header">Posts</div>
 
         <div class="card-body">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Image</th>
+                    <th>Title</th>
+                    <th colspan="2"></th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($posts as $post)
+                <tr>
+                    <td><img src="{{ asset('storage/'.$post->image) }}" width="90" alt=""></td>
+                    <td>{{ $post->title }}</td>
+                    <td><a href="" class="btn btn-info btn-sm text-white">Edit</a></td>
+                    <td>
+                        <form action="{{ route('posts.destroy', $post->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm text-white">Trash</button>
+                        </form>
+
+                    </td>
+                </tr>
+                @endforeach
+                </tbody>
+            </table>
 
         </div>
     </div>
