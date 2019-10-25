ee6f57cb3a5f1e818c40e35b1cb42f5988c0db30
Tochukwu <ugwukelvintochukwu@gmail.com>
2019-10-25 13:10:29 +0100

    create posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Posts\CreatePostsRequest;
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -32,9 +34,27 @@ class PostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreatePostsRequest $request)
     {
-        //
+        // upload the image to storage
+        $image = $request->image->store('posts');
+
+        // create the post
+
+        Post::create([
+            'title'         => $request->title,
+            'description'   => $request->description,
+            'image'         => $image,
+            'content'       => $request->content
+        ]);
+
+        // flash message
+
+        session()->flash('success', 'Post created successfully');
+
+        // redirect user
+
+        return redirect(route('posts.index'));
     }
 
     /**
